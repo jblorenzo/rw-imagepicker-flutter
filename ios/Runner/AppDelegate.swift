e3eed319d85429c78f19d865737e61a5c315f559
jblorenzo <kzer95@gmail.com>
2019-04-15 02:07:26 +0200

    fetch gallery images for ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,60 @@
 import UIKit
 import Flutter
+import Photos
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
-  ) -> Bool {
+    ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    print("image count: \(getGalleryImageCount())")
+    dataForGalleryItem(index: 0) { (data, created, location) in
+      if let data = data {
+        print("first data: \(data) \(created) \(location)")
+      }
+    }
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func dataForGalleryItem(index: Int, completion: @escaping (Data?, Int, String) -> Void) {
+    let fetchOptions = PHFetchOptions()
+
+    let collection: PHFetchResult = PHAsset.fetchAssets(with: fetchOptions)
+    if (index >= collection.count) {
+      return
+    }
+
+    let asset = collection.object(at: index)
+
+    let options = PHImageRequestOptions()
+    options.deliveryMode = .fastFormat
+    options.isSynchronous = true
+
+    let imageSize = CGSize(width: 250,
+                           height: 250)
+
+    let imageManager = PHCachingImageManager()
+    imageManager.requestImage(for: asset, targetSize: imageSize, contentMode: .aspectFit, options: options) { (image, info) in
+      if let image = image {
+        let data = UIImageJPEGRepresentation(image, 0.9)
+        completion(data,
+                   Int(asset.creationDate?.timeIntervalSince1970 ?? 0),
+                   "\(asset.location ?? CLLocation())")
+      } else {
+        completion(nil, 0, "")
+      }
+    }
+  }
+
+  func getGalleryImageCount() -> Int {
+    let fetchOptions = PHFetchOptions()
+    fetchOptions.includeHiddenAssets = true
+
+    let collection: PHFetchResult = PHAsset.fetchAssets(with: fetchOptions)
+    return collection.count
   }
 }
